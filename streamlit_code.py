e8f22b129338935dc3cc1e6776309752a5f5fc76
Felps791 <felipedtm791@hotmail.com>
2024-09-30 14:37:42 +0000

    streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.write("Auba")
+df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+
+st.dataframe(df)  # Same as st.write(df)
+
 st.write("Hello World")
 st.sidebar.button("Side Button")
