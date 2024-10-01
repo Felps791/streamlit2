fa25812b79ed502f66a38e8b32bec8ac7a714dea
Felps791 <felipedtm791@hotmail.com>
2024-10-01 11:28:06 +0000

    streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -1,10 +1,58 @@
 import streamlit as st
-import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import requests
 
-df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+def simples_plot(title,data,rotulo):
 
-st.dataframe(df)  # Same as st.write(df)
+    # Define o tamanho da figura com base nas entradas do usuário
+    plt.figure(figsize=(10, 4))
+    plt.title(f"{title}")
+    plt.scatter(data[:,0], data[:,1], c=rotulo)
+    return plt
 
-st.write("Hello World")
-st.sidebar.button("Side Button")
+st.set_page_config(page_title="Maykoll Rocha - Portifolio Pessoal",
+            page_icon=None ,
+            layout="wide",
+            initial_sidebar_state="collapsed",
+        )
+    
+def main():
+    st.write("Hello Word")
+    st.text("Hello Word")
+    st.latex(r"P(n, k) = \frac{1}{k!} \sum_{j=1}^{k} (-1)^{k-j} \binom{k}{j} j^n ")
+    st.markdown(r"""
+                $$ P(n, k) = \frac{1}{k!} \sum_{j=1}^{k} (-1)^{k-j} \binom{k}{j} j^n $$  
+                maykoll 
+                rocha
+                """)    
+
+    entra = st.number_input("Entra numero")
+    st.write(entra)
+    color  = st.color_picker('Pick a color',)
+    st.write(color)
+
+    data = st.date_input("Entre com o dia")
+    st.write(data)
+def plot():
+        
+    # Baixa os dados dos arquivos
+    data_response = requests.get('https://raw.githubusercontent.com/MaykollRocha/Data_Sets/main/data.txt')
+    rotulo_response = requests.get('https://raw.githubusercontent.com/MaykollRocha/Data_Sets/main/rotulos.txt')
+
+    # Carrega os dados no NumPy
+    data = np.loadtxt(data_response.text.splitlines())
+    rotulo = np.loadtxt(rotulo_response.text.splitlines())
+
+    st.pyplot(simples_plot("Base de Dados Tratada",data,rotulo))
+    
+
+
+
+
+
+if st.sidebar.button("main"):
+    main()
+if st.sidebar.button("plot"):
+    plot()
